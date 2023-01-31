bc9c7781a3f5f0c30a6373138c8e8db73268e9b7
Daniel Grieco <daniel_grieco@programmer.net>
2023-01-31 21:09:08 +0100

    Add files for the Home and Detail screens

---
+++ screens/DetailScreen.js
@@ -0,0 +1,11 @@
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+
+export default function DetailScreen() {
+    return (
+        <View>
+            <StatusBar style="auto" />
+            <Text>Detail Screen</Text>
+        </View>
+    );
+}
