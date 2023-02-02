f0b2e2998ecb34feaffd2ac9451fcfebf95ef457
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-02 20:17:11 +0100

    Add Flatlist with temporary data to the home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,47 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+const TempData = [
+    { id: 1, text: "Pokemon 1" },
+    { id: 2, text: "Pokemon 2" },
+    { id: 3, text: "Pokemon 3" },
+    { id: 4, text: "Pokemon 4" },
+];
 
 export default function HomeScreen() {
     return (
-        <View>
+        <View style={styles.container}>
             <StatusBar style="auto" />
-            <Text>Home Screen</Text>
+
+            <FlatList
+                style={styles.pokemonList}
+                data={TempData}
+                keyExtractor={(data) => data.id}
+                renderItem={({ item }) => (
+                    <Text
+                        style={{
+                            backgroundColor: "#f00",
+                            padding: 24,
+                            marginHorizontal: 16,
+                            marginVertical: 8,
+                            textAlign: "center",
+                        }}
+                    >
+                        {item.text}
+                    </Text>
+                )}
+                numColumns={2}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    pokemonList: {
+        flex: 1,
+        marginTop: 8,
+    },
+});
