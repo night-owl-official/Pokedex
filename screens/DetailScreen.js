f143fff4324f0fe4e4ee15e13b209c92a3419cb6
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-03 22:00:12 +0100

    Set up container for the details component in the Details screen

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,15 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { View, Animated, StyleSheet } from "react-native";
+// import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 import PokemonSummary from "../components/PokemonSummary";
+import PokemonDetails from "../components/PokemonDetails";
 
 export default function DetailScreen({ route }) {
+    // const panGesture = Gesture.Pan();
+
     return (
         <View style={styles.container}>
             <StatusBar style="light" />
 
             <View>
                 <PokemonSummary pokemonData={route.params.pokemonData} />
+
+                <Animated.View style={styles.detailsContainer}>
+                    <PokemonDetails pokemonData={route.params.pokemonData} />
+                </Animated.View>
             </View>
         </View>
     );
