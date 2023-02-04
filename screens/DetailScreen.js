5fb5d8fd5c4368286dafc1361f015476218061b7
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-04 23:42:50 +0100

    Add pan gesture handling to the bottom sheet to make it draggable vertically

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,25 +1,89 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Animated, StyleSheet } from "react-native";
-// import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { useCallback } from "react";
+import { View, StyleSheet } from "react-native";
+import {
+    Gesture,
+    GestureDetector,
+    GestureHandlerRootView,
+} from "react-native-gesture-handler";
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    interpolate,
+    Extrapolate,
+    withSpring,
+} from "react-native-reanimated";
 
 import PokemonSummary from "../components/PokemonSummary";
 import PokemonDetails from "../components/PokemonDetails";
 
+const POKEMON_SUMMARY_HEIGHT = 360;
+
+function GetPanGesture(transY) {
+    const context = useSharedValue({ y: 0 });
+
+    const scrollTo = useCallback((destination) => {
+        "worklet";
+        transY.value = withSpring(destination, { damping: 50 });
+    }, []);
+
+    return Gesture.Pan()
+        .onStart(() => {
+            context.value = { y: transY.value };
+        })
+        .onUpdate((event) => {
+            transY.value = event.translationY + context.value.y;
+        })
+        .onEnd(() => {
+            if (transY.value < -100) scrollTo(-POKEMON_SUMMARY_HEIGHT);
+            else scrollTo(0);
+        });
+}
+
+function GetDetailsContainerAnimatedStyle(transY) {
+    return useAnimatedStyle(() => {
+        const yTranslate = interpolate(
+            transY.value,
+            [-POKEMON_SUMMARY_HEIGHT, 0, 200],
+            [-POKEMON_SUMMARY_HEIGHT, 0, 50],
+            Extrapolate.CLAMP
+        );
+
+        return {
+            transform: [{ translateY: yTranslate }],
+        };
+    });
+}
+
 export default function DetailScreen({ route }) {
-    // const panGesture = Gesture.Pan();
+    const translateY = useSharedValue(0);
+
+    const panGesture = GetPanGesture(translateY);
+    const detailsContainerStyle = GetDetailsContainerAnimatedStyle(translateY);
 
     return (
-        <View style={styles.container}>
-            <StatusBar style="light" />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <StatusBar style="light" />
 
-            <View>
-                <PokemonSummary pokemonData={route.params.pokemonData} />
+                <View>
+                    <PokemonSummary pokemonData={route.params.pokemonData} />
 
-                <Animated.View style={styles.detailsContainer}>
-                    <PokemonDetails pokemonData={route.params.pokemonData} />
-                </Animated.View>
+                    <GestureDetector gesture={panGesture}>
+                        <Animated.View
+                            style={[
+                                styles.detailsContainer,
+                                detailsContainerStyle,
+                            ]}
+                        >
+                            <PokemonDetails
+                                pokemonData={route.params.pokemonData}
+                            />
+                        </Animated.View>
+                    </GestureDetector>
+                </View>
             </View>
-        </View>
+        </GestureHandlerRootView>
     );
 }
 
