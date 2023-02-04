8c443b49e037f3cfdf07d2df28aeb88c57c70cd6
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-04 20:58:50 +0100

    Modify the temp data to pass to the Pokemon List

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,18 +4,14 @@ import { View, StyleSheet, FlatList } from "react-native";
 import PokemonCard from "../components/PokemonCard";
 
 const TempData = [
-    { id: 1, text: "Pokemon 1" },
-    { id: 2, text: "Pokemon 2" },
-    { id: 3, text: "Pokemon 3" },
-    { id: 4, text: "Pokemon 4" },
-    { id: 5, text: "Pokemon 1" },
-    { id: 6, text: "Pokemon 2" },
-    { id: 7, text: "Pokemon 3" },
-    { id: 8, text: "Pokemon 4" },
-    { id: 9, text: "Pokemon 1" },
-    { id: 10, text: "Pokemon 2" },
-    { id: 11, text: "Pokemon 3" },
-    { id: 12, text: "Pokemon 4" },
+    {
+        id: 1,
+        name: "Pokemon",
+        dexNumber: 6,
+        types: ["Type 1", "Type 2"],
+        image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+        genera: "Genera",
+    },
 ];
 
 export default function HomeScreen({ navigation }) {
@@ -26,17 +22,19 @@ export default function HomeScreen({ navigation }) {
             <FlatList
                 style={styles.pokemonList}
                 data={TempData}
-                keyExtractor={(data) => data.id}
+                keyExtractor={(data) => data.dexNumber}
                 renderItem={({ item }) => (
                     <PokemonCard
-                        pokemonName={item.text}
-                        pokemonDexNumber={item.id}
-                        pokemonImage={
-                            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
-                        }
-                        pokemonTypes={["Type 1", "Type 2"]}
+                        pokemonName={item.name}
+                        pokemonDexNumber={item.dexNumber}
+                        pokemonImage={item.image}
+                        pokemonTypes={item.types}
                         bgColor={"#F95D5E"}
-                        onPress={() => navigation.navigate("Details")}
+                        onPress={() =>
+                            navigation.navigate("Details", {
+                                pokemonData: item,
+                            })
+                        }
                     />
                 )}
                 numColumns={2}
