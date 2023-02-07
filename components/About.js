d0390ea28139ea0695d05ac13db9d18d4d3ea2f2
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-07 20:40:53 +0100

    Add styles for the about tab component

--- components/About.js
+++ components/About.js
@@ -1,1 +1,43 @@
+import { StyleSheet, View } from "react-native";
+
 export default function About({ pokemonData }) {}
+
+const styles = StyleSheet.create({
+    section: {
+        marginBottom: 32,
+    },
+    sectionTitle: {
+        marginBottom: 8,
+        fontWeight: "bold",
+        fontSize: 18,
+        lineHeight: 22,
+    },
+    sectionContent: {
+        marginTop: 16,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    sectionSubtitle: {
+        width: 100,
+        color: "#919191",
+    },
+    sectionText: {
+        fontWeight: "bold",
+    },
+    shadowContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 24,
+        borderRadius: 16,
+        backgroundColor: "#fff",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 2,
+            height: 2,
+        },
+        shadowOpacity: 0.39,
+        shadowRadius: 4.65,
+
+        elevation: 5,
+    },
+});
