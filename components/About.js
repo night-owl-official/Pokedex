8a8073b2f41076f77e20945eee9f054684029fa8
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-07 22:24:37 +0100

    Add Breeding section components to the about tab

--- components/About.js
+++ components/About.js
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Animated } from "react-native";
+import { Foundation as Icon } from "@expo/vector-icons";
 
 export default function About({ pokemonData }) {
     return (
@@ -35,6 +36,89 @@ export default function About({ pokemonData }) {
                     </View>
                 </View>
             </View>
+
+            {/* Breeding */}
+            <View style={styles.section}>
+                {/* Title */}
+                <Animated.Text style={styles.sectionTitle}>
+                    Breeding
+                </Animated.Text>
+
+                {/* Gender ratio */}
+                <View style={styles.sectionContent}>
+                    {/* Subtitle */}
+                    <Animated.Text style={styles.sectionSubtitle}>
+                        Gender
+                    </Animated.Text>
+
+                    {/* Display Genders */}
+                    {pokemonData.gender.map((gender, index) => (
+                        <Animated.Text
+                            style={[styles.sectionText, { marginRight: 16 }]}
+                            key={index}
+                        >
+                            {gender.gender === "Genderless" ? (
+                                // Genderless Text
+                                <Animated.Text style={{ fontWeight: "bold" }}>
+                                    Genderless
+                                </Animated.Text>
+                            ) : (
+                                // Gender and symbol
+                                <>
+                                    <Icon
+                                        name={
+                                            gender.gender === "Male"
+                                                ? "male-symbol"
+                                                : "female-symbol"
+                                        }
+                                        color={
+                                            gender.gender === "Male"
+                                                ? "#6890F0"
+                                                : "#EE99AC"
+                                        }
+                                        size={16}
+                                    />
+                                    {"  "}
+                                    {gender.rate}%
+                                </>
+                            )}
+                        </Animated.Text>
+                    ))}
+                </View>
+
+                {/* Egg groups */}
+                <View style={styles.sectionContent}>
+                    {/* Subtitle */}
+                    <Animated.Text style={styles.sectionSubtitle}>
+                        Egg Groups
+                    </Animated.Text>
+
+                    {/* Display Egg groups */}
+                    {pokemonData.eggGroups.map((eggGroup, index) => (
+                        <Animated.Text
+                            key={index}
+                            style={[styles.sectionText, { marginRight: 8 }]}
+                        >
+                            {eggGroup}
+                        </Animated.Text>
+                    ))}
+                </View>
+
+                {/* Base Egg steps */}
+                <View style={styles.sectionContent}>
+                    {/* Subtitle */}
+                    <Animated.Text style={styles.sectionSubtitle}>
+                        Base Egg Steps
+                    </Animated.Text>
+
+                    {/* Display Base Egg Steps */}
+                    <Animated.Text
+                        style={[styles.sectionText, { marginRight: 8 }]}
+                    >
+                        {pokemonData.baseEggSteps}
+                    </Animated.Text>
+                </View>
+            </View>
         </>
     );
 }
@@ -44,7 +128,6 @@ const styles = StyleSheet.create({
         marginBottom: 32,
     },
     sectionTitle: {
-        marginBottom: 8,
         fontWeight: "bold",
         fontSize: 18,
         lineHeight: 22,
