2858ec0ea5e15a3b5b25fa8be7bb0679fcc11664
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-08 02:22:55 +0100

    Add more data to the temp pokemon data in the Home screen to test out about component

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,33 @@ const TempData = [
         types: ["Type 1", "Type 2"],
         image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
         genera: "Genera",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque diam tellus, ullamcorper mollis leo nec, scelerisque tempor massa.",
+        height: "1.70 m (5.58 ft)",
+        weight: "90.50 kg (199.52 lbs)",
+        gender: [
+            { gender: "Male", rate: 87.5 },
+            { gender: "Female", rate: 12.5 },
+        ],
+        eggGroups: ["Monster", "Dragon"],
+        baseEggSteps: 5120,
+        baseExp: 133,
+        baseHappiness: 20,
+        captureRate: 10,
+        abilities: [
+            {
+                name: "Ability 1",
+                description:
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque diam tellus, ullamcorper mollis leo nec, scelerisque tempor massa.",
+                type: "Ability 1",
+            },
+            {
+                name: "Hidden",
+                description:
+                    "Lorem ipsum dolor sit HIDDEN, consectetur adipiscing elit. Quisque diam tellus, ullamcorper mollis leo nec, scelerisque tempor massa.",
+                type: "Hidden",
+            },
+        ],
     },
 ];
 
