8cf827d73bc0cabec67cfa18b13e0f93e051f8e5
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-08 17:13:45 +0100

    Add components and styles to show base stats in the base stats tab

--- components/BaseStats.js
+++ components/BaseStats.js
@@ -1,1 +1,80 @@
-export default function BaseStats({ pokemonData }) {}
+import { StyleSheet, View, Animated } from "react-native";
+
+export default function BaseStats({ pokemonData }) {
+    return (
+        <>
+            {pokemonData.baseStats.map((baseStat, index) => (
+                <View key={index} style={styles.statWrapper}>
+                    {/* Stat Name */}
+                    <Animated.Text style={{ color: "#919191", width: 100 }}>
+                        {baseStat.name}
+                    </Animated.Text>
+
+                    {/* Stat Graph */}
+                    <View style={styles.statGraphWrapper}>
+                        {/* Number */}
+                        <Animated.Text
+                            style={{
+                                fontWeight: "bold",
+                                textAlign: "right",
+                                width: 30,
+                            }}
+                        >
+                            {baseStat.value}
+                        </Animated.Text>
+
+                        {/* Slider */}
+                        <View style={styles.statLine}>
+                            <Animated.View
+                                style={[
+                                    { width: baseStat.value },
+                                    baseStat.value >= 50
+                                        ? baseStat.value < 60
+                                            ? styles.statValueMid
+                                            : styles.statValueHigh
+                                        : styles.statValueLow,
+                                ]}
+                            />
+                        </View>
+                    </View>
+                </View>
+            ))}
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    statWrapper: {
+        marginBottom: 16,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    statGraphWrapper: {
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    statLine: {
+        flex: 1,
+        overflow: "hidden",
+        height: 5,
+        borderRadius: 10,
+        marginLeft: 16,
+        backgroundColor: "#E7E7E8",
+    },
+    statValueHigh: {
+        borderRadius: 10,
+        height: 5,
+        backgroundColor: "#41D168",
+    },
+    statValueMid: {
+        borderRadius: 10,
+        height: 5,
+        backgroundColor: "#fff53a",
+    },
+    statValueLow: {
+        borderRadius: 10,
+        height: 5,
+        backgroundColor: "#FA6555",
+    },
+});
