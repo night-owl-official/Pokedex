aba995099e86fe1a732357cd3c80a0d878cb437c
Daniel Grieco <daniel_grieco@programmer.net>
2023-02-10 23:55:40 +0100

    Split stack navigator screens into a separate component

--- App.js
+++ App.js
@@ -7,42 +7,48 @@ import DetailScreen from "./screens/DetailScreen";
 
 const Stack = createStackNavigator();
 
+function Screens() {
+    return (
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    title: "POKEDEX",
+                    headerTitleAlign: "center",
+                    headerTitleStyle: {
+                        fontSize: 28,
+                        fontWeight: "bold",
+                        color: "#F1869A",
+                    },
+                    headerStyle: {
+                        backgroundColor: "#fff",
+                    },
+                    headerShadowVisible: false,
+                }}
+            />
+            <Stack.Screen
+                name="Details"
+                component={DetailScreen}
+                options={({ route }) => ({
+                    title: "",
+                    headerTitleAlign: "center",
+                    headerShadowVisible: false,
+                    headerBackTitleVisible: false,
+                    headerTintColor: "#fff",
+                    headerStyle: {
+                        backgroundColor: route.params.bgColor,
+                    },
+                })}
+            />
+        </Stack.Navigator>
+    );
+}
+
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        title: "POKEDEX",
-                        headerTitleAlign: "center",
-                        headerTitleStyle: {
-                            fontSize: 28,
-                            fontWeight: "bold",
-                            color: "#F1869A",
-                        },
-                        headerStyle: {
-                            backgroundColor: "#fff",
-                        },
-                        headerShadowVisible: false,
-                    }}
-                />
-                <Stack.Screen
-                    name="Details"
-                    component={DetailScreen}
-                    options={({ route }) => ({
-                        title: "",
-                        headerTitleAlign: "center",
-                        headerShadowVisible: false,
-                        headerBackTitleVisible: false,
-                        headerTintColor: "#fff",
-                        headerStyle: {
-                            backgroundColor: route.params.bgColor,
-                        },
-                    })}
-                />
-            </Stack.Navigator>
+            <Screens />
         </NavigationContainer>
     );
 }
